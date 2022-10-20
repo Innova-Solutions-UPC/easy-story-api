62e2191a6084b757afe0605fc00bbe1591941267
Marco Antonio Manrique Acha <63822742+MarcoMnrq@users.noreply.github.com>
2022-10-20 11:20:06 -0500

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   });
   /* Enabling CORS for the application. */
   app.enableCors({
-    origin: '*',
+    origin: 'http://127.0.0.1:5173',
     methods: 'GET,HEAD,PATCH,POST,DELETE,OPTIONS',
   });
   /* Configuring validation pipes */
